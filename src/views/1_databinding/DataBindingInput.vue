b4bd2dfe9f90d0b05cc70a12aa6705cd6e38cc7e
Seungwon Go <seungwon.go@gmail.com>
2021-07-09 11:20:36 +0900

    수업 예제 파일(코드작성전) 생성

---
+++ src/views/1_databinding/DataBindingInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text" v-model="valueModel" />
+    <input type="number" v-model.number="numberModel" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      valueModel: 'South Korea',
+      numberModel: 3
+    }
+  }
+}
+</script>
