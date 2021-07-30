e8b2b90912769b66b905f0a065da2bb5889f2f4d
Seungwon Go <seungwon.go@gmail.com>
2021-07-30 11:00:48 +0900

    update

--- src/router/index.js
+++ src/router/index.js
@@ -18,107 +18,168 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/databinding/string',
     name: 'DataBindingString',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingString.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingString.vue'
+      )
   },
   {
     path: '/databinding/html',
     name: 'DataBindingHtml',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingHtml.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingHtml.vue'
+      )
   },
   {
     path: '/databinding/input',
     name: 'DataBindingInput',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingInput.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingInput.vue'
+      )
   },
   {
     path: '/databinding/checkbox',
     name: 'DataBindingCheckbox',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingCheckbox.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingCheckbox.vue'
+      )
   },
   {
     path: '/databinding/radio',
     name: 'DataBindingRadio',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingRadio.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingRadio.vue'
+      )
   },
   {
     path: '/databinding/select',
     name: 'DataBindingSelect',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingSelect.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingSelect.vue'
+      )
   },
   {
     path: '/databinding/textarea',
     name: 'DataBindingTextarea',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingTextarea.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingTextarea.vue'
+      )
   },
   {
     path: '/databinding/attribute',
     name: 'DataBindingAttribute',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingAttribute.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingAttribute.vue'
+      )
   },
   {
     path: '/databinding/class',
     name: 'DataBindingClass',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingClass.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingClass.vue'
+      )
   },
   {
     path: '/databinding/style',
     name: 'DataBindingStyle',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingStyle.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingStyle.vue'
+      )
   },
   {
     path: '/databinding/list',
     name: 'DataBindingList',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingList.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingList.vue'
+      )
   },
   {
     path: '/databinding/listserver',
     name: 'DataBindingListServer',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingListServer.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "databinding" */ '../views/1_databinding/DataBindingListServer.vue'
+      )
   },
   {
     path: '/event/click',
     name: 'EventClick',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "event" */ '../views/2_event/EventClick.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "event" */ '../views/2_event/EventClick.vue'
+      )
   },
   {
     path: '/event/change',
     name: 'EventChange',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "event" */ '../views/2_event/EventChange.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "event" */ '../views/2_event/EventChange.vue'
+      )
   },
   {
     path: '/event/blur',
     name: 'EventBlur',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "event" */ '../views/2_event/EventBlur.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "event" */ '../views/2_event/EventBlur.vue'
+      )
   },
   {
     path: '/event/key',
     name: 'EventKey',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "event" */ '../views/2_event/EventKey.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "event" */ '../views/2_event/EventKey.vue'
+      )
   },
   {
     path: '/extra/renderingif',
     name: 'RenderingIf',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "event" */ '../views/3_extra/RenderingIf.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "event" */ '../views/3_extra/RenderingIf.vue'
+      )
   },
   {
     path: '/extra/renderingshow',
     name: 'RenderingShow',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "event" */ '../views/3_extra/RenderingShow.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "event" */ '../views/3_extra/RenderingShow.vue'
+      )
   },
   {
     path: '/extra/computed',
     name: 'Computed',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "event" */ '../views/3_extra/Computed.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "event" */ '../views/3_extra/Computed.vue'
+      )
   },
   {
     path: '/extra/watch',
     name: 'Watch',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "event" */ '../views/3_extra/Watch.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "event" */ '../views/3_extra/Watch.vue'
+      )
   },
   {
     path: '/reuse/reusecomponent',
@@ -148,82 +209,138 @@ const routes = [
   {
     path: '/advanced/mixin',
     name: 'Mixin',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/Mixin.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/Mixin.vue'
+      )
   },
   {
     path: '/advanced/plugin',
     name: 'Plugin',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/Plugin.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/Plugin.vue'
+      )
   },
   {
     path: '/advanced/composition',
     name: 'CompositionAPI',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPI.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPI.vue'
+      )
   },
   {
     path: '/advanced/composition2',
     name: 'CompositionAPI2',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPI2.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPI2.vue'
+      )
   },
   {
     path: '/advanced/composition3',
     name: 'CompositionAPI3',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPI3.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPI3.vue'
+      )
   },
   {
     path: '/advanced/composition4',
     name: 'CompositionAPI4',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPI4.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPI4.vue'
+      )
   },
   {
     path: '/advanced/compositionprovide',
     name: 'CompositionAPIProvide',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPIProvide.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "advanced" */ '../views/5_advanced/CompositionAPIProvide.vue'
+      )
   },
   {
     path: '/vuex/store',
     name: 'StoreAccess',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "vuex" */ '../views/6_vuex/StoreAccess.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "vuex" */ '../views/6_vuex/StoreAccess.vue'
+      )
   },
   {
     path: '/materialmodal',
     name: 'MaterialModal',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "modal" */ '../components/erp/MaterialModal.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "modal" */ '../components/erp/MaterialModal.vue'
+      )
   },
   {
     path: '/template/listtodetail',
     name: 'ListToDetail',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/ListToDetail.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/ListToDetail.vue'
+      )
   },
   {
     path: '/template/detail',
     name: 'Detail',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/Detail.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/Detail.vue'
+      )
   },
   {
     path: '/template/detailedit',
     name: 'DetailEdit',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/DetailEdit.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/DetailEdit.vue'
+      )
   },
   {
     path: '/template/multipleedit',
     name: 'MultipleEdit',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/MultipleEdit.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/MultipleEdit.vue'
+      )
   },
   {
     path: '/template/masterdetail',
     name: 'MasterDetail',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/MasterDetail.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/MasterDetail.vue'
+      )
   },
   {
     path: '/template/shuttle',
     name: 'Shuttle',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/Shuttle.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/Shuttle.vue'
+      )
   },
   {
     path: '/template/singleviewedit',
     name: 'SingleViewEdit',
-    component: () => import(/* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/SingleViewEdit.vue')
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/9_uitemplate/SingleViewEdit.vue'
+      )
+  },
+  {
+    path: '/assignment/usercreate',
+    name: 'UserCreate',
+    component: () =>
+      import(
+        /* webpackPrefetch:true, webpackChunkName: "uitemplate" */ '../views/8_assignment/UserCreate.vue'
+      )
   }
 ]
 
