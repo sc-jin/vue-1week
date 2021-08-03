7167921cfcdc1737cdb0940eaa011a0a8272308b
Seungwon Go <seungwon.go@gmail.com>
2021-08-03 16:59:21 +0900

    Update CustomDirective.vue

--- src/views/5_advanced/CustomDirective.vue
+++ src/views/5_advanced/CustomDirective.vue
@@ -1,13 +1,72 @@
 <template>
-  <input type="text" v-focus />
+  <div>
+    <div><input type="text" v-focus /></div>
+    <div><input type="text" v-blur:[required] /></div>
+    <div><input type="text" v-number /></div>
+    <div><input type="text" v-float /></div>
+  </div>
 </template>
 <script>
 export default {
   directives: {
     focus: {
-      mounted(el) {
+      mounted(el, binding) {
+        console.log(el)
+        console.log(binding)
         el.focus()
       }
+    },
+    blur: {
+      mounted(el, binding) {
+        if (binding.arg) {
+          el.addEventListener('blur', () => {
+            if (el.value === '') {
+              alert('필수 입력 값입니다.')
+            }
+          })
+        }
+      }
+    },
+    number: {
+      mounted(el, binding) {
+        el.addEventListener('keydown', e => {
+          if (
+            !(
+              (e.keyCode >= 48 && e.keyCode <= 57) ||
+              e.keyCode === 8 ||
+              e.keyCode === 46 ||
+              e.keyCode === 37 ||
+              e.keyCode === 39
+            )
+          ) {
+            e.preventDefault()
+          }
+        })
+      }
+    },
+    float: {
+      mounted(el, binding) {
+        el.addEventListener('keydown', e => {
+          if (
+            !(
+              (e.keyCode >= 48 && e.keyCode <= 57) ||
+              e.keyCode === 190 ||
+              e.keyCode === 8 ||
+              e.keyCode === 46 ||
+              e.keyCode === 37 ||
+              e.keyCode === 39
+            )
+          ) {
+            e.preventDefault()
+          }
+        })
+      }
+    }
+  },
+  data() {
+    return {
+      required: true,
+      txt: 'abc'
     }
   }
 }
