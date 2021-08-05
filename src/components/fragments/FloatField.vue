a60fddc3aad6f3a27680b59d7f002fbe54a2e6f4
Seungwon Go <seungwon.go@gmail.com>
2021-08-05 14:25:01 +0900

    Create FloatField.vue

---
+++ src/components/fragments/FloatField.vue
@@ -0,0 +1,34 @@
+<template>
+  <input type="text" @keydown="checkFloat(event)" />
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    checkFloat(e) {
+      if (
+        !(
+          (e.keyCode >= 48 && e.keyCode <= 57) ||
+          e.keyCode === 190 ||
+          e.keyCode === 8 ||
+          e.keyCode === 46 ||
+          e.keyCode === 37 ||
+          e.keyCode === 39
+        )
+      ) {
+        e.preventDefault()
+      }
+    }
+  }
+}
+</script>
