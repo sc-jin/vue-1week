29147312e1cb6e2d58f0262f8ea4c7be9ed033ad
Seungwon Go <seungwon.go@gmail.com>
2021-08-05 14:25:06 +0900

    Create NumberField.vue

---
+++ src/components/fragments/NumberField.vue
@@ -0,0 +1,33 @@
+<template>
+  <input type="text" @keydown="checkInt(event)" />
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    checkInt(e) {
+      if (
+        !(
+          (e.keyCode >= 48 && e.keyCode <= 57) ||
+          e.keyCode === 8 ||
+          e.keyCode === 46 ||
+          e.keyCode === 37 ||
+          e.keyCode === 39
+        )
+      ) {
+        e.preventDefault()
+      }
+    }
+  }
+}
+</script>
